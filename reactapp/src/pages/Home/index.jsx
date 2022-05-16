bcaf4ef54bbaa9032c41426adf81d19d70c3f5b2
Deviego <diegodomingues266@gmail.com>
2022-05-16 14:17:09 -0300

    first  commit

---
+++ reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+
+import './style.css'
+
+export function Home() {
+  return (
+    <div id='Home'>
+    <h1>Hello world </h1>
+      <input type="text" placeholder="Digite um nome" />
+      <button type="Button">Adicionar</button>
+    </div>
+    
+  )
+  
+}
+
+
